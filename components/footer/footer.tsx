a3c046fd34e6f07de125939cfbc4b64137e2cab2
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-02 18:58:41 +0300

    feat: add footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import RssLogo from '@/logos/rss-logo.svg'
+import { AUTHOR_GITHUBS } from "@/constants";
+
+export const Footer = () => {
+    return (
+        <footer className="w-full bg-glass backdrop-blur-md border-t border-bg-300 py-4 text-center text-muted text-sm"><div className="container mx-auto flex flex-col md:flex-row items-center justify-between py-4 px-6">
+        
+        <div className="flex gap-4">
+            {AUTHOR_GITHUBS.map(author => 
+          <Link 
+            href={author.url}
+            target="_blank" 
+            className="hover:text-gray-900"
+          >
+            {author.name}
+          </Link>
+        )}
+        </div>
+
+        <span className="text-sm">&copy; 2025 REST Client App </span>
+
+        <Link href="https://rs.school/courses/reactjs" target="_blank">
+        6666
+        </Link>
+      </div>
+  </footer>
+    );
+}
+
---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+export const AUTHOR_GITHUBS: {name: string; url: string}[] = [
+    {name: 'Anastasiia', url: ''},
+    {name: 'Ekaterina', url: ''},
+    {name: 'Yulia', url: ''},
+];
+
+export const locales = ['en', 'ru', 'fr'] as const;
+export type Locale = (typeof locales)[number];
