02d95f6dcb7559f5dba92a7ca170feb4dfc55d56
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-02 18:58:54 +0300

    fix: locale layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,22 @@
-export default function LocaleLayout({
+import { Footer } from '@/components';
+import type { Locale } from '@/constants';
+
+import type { ReactNode } from 'react';
+
+export default async function RootLayout({
   children,
   params,
-}: Readonly<{
-  children: React.ReactNode;
-  params: { locale: string };
-}>) {
-  const { locale } = params;
+}: {
+  children: ReactNode;
+  params: Promise<{ locale: Locale }>;
+}) {
+
   return (
-    <html lang={locale || 'en'}>
-      <body>{children}</body>
+    <html lang={(await params).locale || 'en'}>
+      <body>
+          {children}
+          <Footer />
+      </body>
     </html>
   );
 }
