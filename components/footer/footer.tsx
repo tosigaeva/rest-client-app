99db8ffc69adf74e22ecadb5aec408b363169af0
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-02 20:08:57 +0300

    fix: formatter

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,7 +8,12 @@ export const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between py-4 px-6">
         <div className="flex gap-4">
           {AUTHOR_GITHUBS.map((author) => (
-            <Link key={author.name} href={author.url} target="_blank" className="hover:text-gray-900">
+            <Link
+              key={author.name}
+              href={author.url}
+              target="_blank"
+              className="hover:text-gray-900"
+            >
               {author.name}
             </Link>
           ))}
@@ -17,7 +22,7 @@ export const Footer = () => {
         <span className="text-sm">&copy; 2025 REST Client App </span>
 
         <Link href={RSS_LINK} target="_blank">
-          <Image src=".logos/rss-logo.svg" alt="RS School"/>
+          <Image src=".logos/rss-logo.svg" alt="RS School" />
         </Link>
       </div>
     </footer>
