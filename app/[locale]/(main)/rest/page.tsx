ee2c5468713f59e723774c0bac0d75188c6305a7
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-03 16:35:11 +0300

    feat: implement method select

--- app/[locale]/(main)/rest/page.tsx
+++ app/[locale]/(main)/rest/page.tsx
@@ -1,3 +1,30 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+
 export default function RestClientPage() {
-  return <></>;
+  return (
+    <div className="rest-client-page">
+      <div className="client-header">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Method" />
+          </SelectTrigger>
+          <SelectContent>
+            {HTTP_METHODS.map((item) => (
+              <SelectItem key={item} value={item}>
+                {item}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
 }
