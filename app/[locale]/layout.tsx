3eaa05c736b22357e45e98e2299147d48bebf88b
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-03 15:33:16 +0200

    chore: fix-build

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,13 +1,11 @@
 import type { ReactNode } from 'react';
 
-import type { Locale } from '@/constants';
-
 export default async function LocaleLayout({
   children,
   params,
 }: {
   children: ReactNode;
-  params: Promise<{ locale: Locale }>;
+  params: Promise<{ locale: string }>;
 }) {
   return (
     <html lang={(await params).locale || 'en'}>
