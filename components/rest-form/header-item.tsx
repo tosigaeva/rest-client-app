faddb8d8ca6ba2a254af2daffccab64ed8cb9dcf
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-04 16:17:54 +0300

    style: add basic styles

--- components/rest-form/header-item.tsx
+++ components/rest-form/header-item.tsx
@@ -10,8 +10,8 @@ export const HeaderItem = ({
   onDelete: (header: Header) => void;
 }) => {
   return (
-    <div className="flex items-center justify-between gap-4 rounded-md border p-2 shadow-sm">
-      <div className="flex gap-2">
+    <div className="flex items-center justify-between gap-4 rounded-lg border border-neutral-200 bg-white p-3 shadow-sm dark:bg-neutral-800">
+      <div className="flex gap-2 text-sm text-neutral-700 dark:text-neutral-200">
         <span className="font-semibold">{header.headerKey}</span>
         <span>:</span>
         <span>{header.value}</span>
