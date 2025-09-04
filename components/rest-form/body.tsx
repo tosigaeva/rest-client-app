051c55e1010a2bfc09dfbc4dd6124b5882b9ff0d
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-04 15:18:07 +0300

    feat: add textarea and button to the body

--- components/rest-form/body.tsx
+++ components/rest-form/body.tsx
@@ -1,16 +1,23 @@
 'use client';
 import { useState } from 'react';
 
+import { Button } from '../ui/button';
+import { Textarea } from '../ui/textarea';
+
+function handleSendRequest() {}
+
 export const Body = () => {
   const [body, setBody] = useState(() => '');
   return (
     <div className="body-section">
       <h3>Body</h3>
-      <textarea
+      <Textarea
         onChange={(e) => setBody(e.target.value)}
         placeholder="JSON or Text body"
         value={body}
       />
+
+      <Button onClick={handleSendRequest}>Send</Button>
     </div>
   );
 };
