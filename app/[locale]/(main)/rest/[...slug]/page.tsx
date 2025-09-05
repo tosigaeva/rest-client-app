9df71e6345f3bc45ff4213e225ef3ede48befff7
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-05 19:44:04 +0300

    fix: creating url

---
+++ app/[locale]/(main)/rest/[...slug]/page.tsx
@@ -0,0 +1,5 @@
+import { RestResponce } from '@/components/rest-form/responce';
+
+export default function RestClientPage() {
+  return <RestResponce />;
+}
--- utils/set-query-params.ts
+++ utils/set-query-params.ts
@@ -1,14 +1,24 @@
 import { encode } from 'js-base64';
 
-import { Header, HttpMethod } from '@/type';
+import { Header, HttpMethod, Locale } from '@/type';
 
-export function setQueryParams(method: HttpMethod, url: string, body: string, headers: Header[]) {
+export function setQueryParams(
+  locale: Locale,
+  method: HttpMethod,
+  url: string,
+  body: string,
+  headers: Header[],
+) {
   const encodedUrl = encode(url);
   const encodedBody = body ? encode(body) : undefined;
 
-  const query = new URLSearchParams();
-  headers.forEach(({ headerKey, value }) => query.set(headerKey, encodeURIComponent(value)));
-  if (encodedBody) query.set('body', encodedBody);
+  let path = `/${locale}/rest/${method}/${encodedUrl}`;
+  if (encodedBody) {
+    path += `/${encodedBody}`;
+  }
 
-  return `/${method}/${encodedUrl}?${query.toString()}`;
+  const query = new URLSearchParams();
+  headers.forEach(({ headerKey, value }) => query.append(headerKey, encode(value)));
+
+  return `${path}?${query.toString()}`;
 }
