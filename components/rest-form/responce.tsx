7415740cd47f0acb418af26c9f6bb8dd50303434
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-05 08:14:22 +0300

    fix: improve json body

--- components/rest-form/responce.tsx
+++ components/rest-form/responce.tsx
@@ -1,3 +1,5 @@
+import { BodyEditor } from './json-editor';
+
 export const RestResponce = () => {
   return (
     <div className="space-y-3 rounded-2xl border border-neutral-200 bg-neutral-50 p-4 shadow dark:bg-neutral-900">
@@ -7,9 +9,9 @@ export const RestResponce = () => {
       </div>
       <div className="flex items-start gap-2 text-sm">
         <h3 className="font-semibold text-neutral-800 dark:text-neutral-100">Body:</h3>
-        <pre className="break-words whitespace-pre-wrap text-neutral-700 dark:text-neutral-300">
-          23456789876543
-        </pre>
+        <div className="flex-1">
+          <BodyEditor initialBody='{"jkj":6666}' readOnly />
+        </div>
       </div>
     </div>
   );
