998aa1b77365dbc854576b95d26e2b56dcbd2ed7
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-07 21:44:06 +0200

    refactor: replace inputs with floating-input

--- app/[locale]/(main)/(auth)/sign-up/page.tsx
+++ app/[locale]/(main)/(auth)/sign-up/page.tsx
@@ -3,9 +3,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
+import { FloatingInput } from '@/components/floating-input/floating-input';
 import { Button } from '@/components/ui';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui';
-import { cn } from '@/lib/utils';
+import { Form } from '@/components/ui';
 import { SignUpFormData, useValidationSchemas } from '@/lib/validation-auth';
 
 export default function SignUpPage() {
@@ -30,132 +30,26 @@ export default function SignUpPage() {
         <h2 className="mb-10 text-center text-[40px]">New Account</h2> {/*text-4xl = 36px*/}
         <Form {...form}>
           <form className="flex flex-col" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="mb-3">
-              <div className="relative text-base">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <input
-                          placeholder=""
-                          type="text"
-                          {...field}
-                          className={cn(
-                            'peer w-full rounded-sm border px-3.5 pt-5 pb-2 outline-none focus:border-black',
-                            {
-                              'border-gray-300': !form.formState.errors.name,
-                              'border-red-500 focus:border-red-500': form.formState.errors.name,
-                            },
-                          )}
-                        />
-                      </FormControl>
-                      <FormLabel
-                        className={cn(
-                          'peer-focus: peer-focus: peer-focus: absolute top-3 left-3.5 bg-white px-1 text-[12px] text-[#979797] peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-black',
-                          {
-                            '-top-2.5 border-gray-300 text-sm': field.value,
-                            'top-3 text-base': !field.value,
-                          },
-                        )}
-                      >
-                        Name
-                      </FormLabel>
-                      {form.formState.errors.name && (
-                        <FormMessage className="absolute top-0 right-0 rounded-tr-sm bg-red-500 px-1 text-[10px] text-white">
-                          {form.formState.errors.name.message}
-                        </FormMessage>
-                      )}
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-            <div className="mb-3">
-              <div className="relative text-base">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <input
-                          placeholder=""
-                          type="email"
-                          {...field}
-                          className={cn(
-                            'peer w-full rounded-sm border px-3.5 pt-5 pb-2 outline-none focus:border-black',
-                            {
-                              'border-gray-300': !form.formState.errors.email,
-                              'border-red-500 focus:border-red-500': form.formState.errors.email,
-                            },
-                          )}
-                        />
-                      </FormControl>
-                      <FormLabel
-                        className={cn(
-                          'peer-focus: peer-focus: peer-focus: absolute top-3 left-3.5 bg-white px-1 text-[12px] text-[#979797] peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-black',
-                          {
-                            '-top-2.5 border-gray-300 text-sm': field.value,
-                            'top-3 text-base': !field.value,
-                          },
-                        )}
-                      >
-                        Email
-                      </FormLabel>
-                      {form.formState.errors.email && (
-                        <FormMessage className="absolute top-0 right-0 rounded-tr-sm bg-red-500 px-1 text-[10px] text-white">
-                          {form.formState.errors.email.message}
-                        </FormMessage>
-                      )}
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-            <div className="mb-3">
-              <div className="relative text-base">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <input
-                          placeholder=""
-                          type="password"
-                          {...field}
-                          className={cn(
-                            'peer w-full rounded-sm border px-3.5 pt-5 pb-2 outline-none focus:border-black',
-                            {
-                              'border-gray-300': !form.formState.errors.password,
-                              'border-red-500 focus:border-red-500': form.formState.errors.password,
-                            },
-                          )}
-                        />
-                      </FormControl>
-                      <FormLabel
-                        className={cn(
-                          'peer-focus: peer-focus: peer-focus: absolute top-3 left-3.5 bg-white px-1 text-[12px] text-[#979797] peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-black',
-                          {
-                            '-top-2.5 border-gray-300 text-sm': field.value,
-                            'top-3 text-base': !field.value,
-                          },
-                        )}
-                      >
-                        Password
-                      </FormLabel>
-                      {form.formState.errors.password && (
-                        <FormMessage className="absolute top-0 right-0 rounded-tr-sm bg-red-500 px-1 text-[10px] text-white">
-                          {form.formState.errors.password.message}
-                        </FormMessage>
-                      )}
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
+            <FloatingInput
+              control={form.control}
+              error={form.formState.errors.name}
+              label="Name"
+              name="name"
+            />
+            <FloatingInput
+              control={form.control}
+              error={form.formState.errors.email}
+              label="Email"
+              name="email"
+              type="email"
+            />
+            <FloatingInput
+              control={form.control}
+              error={form.formState.errors.password}
+              label="Password"
+              name="password"
+              type="password"
+            />
             <div className="flex flex-wrap">
               <div className="w-1/2 pt-0.5 text-sm text-[#5f5f5f]">
                 <i></i>
