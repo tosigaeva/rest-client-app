c7a20bac442eb038b6f3c406ae9fdbfc3010b8cc
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-09-08 08:38:00 +0300

    fix: fix Spinner

--- components/spinner/spinner.tsx
+++ components/spinner/spinner.tsx
@@ -1,28 +1,22 @@
-'use client';
-
 import React from 'react';
 
-type SpinnerProps = {
-  size?: 'lg' | 'md' | 'sm';
-};
+import { cn } from '@/lib/utils';
 
-export const Spinner = ({ size = 'md' }: SpinnerProps) => {
-  const sizeClasses = {
-    lg: 'h-24 w-24',
-    md: 'h-16 w-16',
-    sm: 'h-10 w-10',
-  };
-
+export const Spinner = () => {
   return (
     <section className="flex h-80 items-center justify-center">
-      <div className={`relative ${sizeClasses[size]}`}>
+      <div className="relative h-16 w-16">
         <div
-          aria-label={'Loaded'}
-          className={`absolute h-full w-full rounded-full border-4 border-gray-200 border-t-blue-500 ${''}`}
+          aria-label="Loading..."
+          className={cn(
+            'absolute h-full w-full',
+            'animate-spin rounded-full',
+            'border-4 border-gray-200 border-t-blue-500',
+          )}
           role="status"
         />
         <div className="absolute inset-0 flex items-center justify-center rounded-full bg-white/90">
-          <span className="font-bold text-blue-500">{'Ready'}</span>
+          <span className="font-bold text-blue-500">Loading...</span>
         </div>
       </div>
     </section>
