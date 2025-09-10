870e6c9852a09e22413d25478b25e2a4c1a9a4ef
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-09-10 23:12:43 +0300

    feat: add translation for variables-block

--- components/variables-block/variables-block.tsx
+++ components/variables-block/variables-block.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { GrSave, GrTrash } from 'react-icons/gr';
+import { useTranslations } from 'use-intl';
 
 import { Button } from '../ui';
 import { Input } from '../ui/input';
@@ -8,6 +9,7 @@ import { getVariables, setVariables } from './storage/storage';
 import { useVariables } from './ui-handles/ui-handles';
 
 export const VariablesBlock = () => {
+  const t = useTranslations('variablesBlock');
   const { handleDelete, handleInputChange, handleSave, rows } = useVariables(
     getVariables,
     setVariables,
@@ -16,21 +18,21 @@ export const VariablesBlock = () => {
   return (
     <section className="mt-35 flex flex-col items-center justify-center gap-10 p-4">
       <div className="flex w-full justify-between">
-        <h3 className="mb-4 text-5xl font-semibold">Variables</h3>
+        <h3 className="mb-4 text-5xl font-semibold">{t('variables')}</h3>
         <Button
           className="cursol-pointer flex justify-between self-end"
           onClick={handleSave}
           title={`Click To SAVE Variables`}
         >
-          Save <GrSave />
+          {t('btnSave')} <GrSave />
         </Button>
       </div>
       <div className="w-full">
         <table className="w-full border-collapse">
           <thead>
             <tr>
-              <th className="border p-2 text-left">Variable</th>
-              <th className="border p-2 text-left">Value</th>
+              <th className="border p-2 text-left">{t('variables')}</th>
+              <th className="border p-2 text-left">{t('value')}</th>
             </tr>
           </thead>
           <tbody>
