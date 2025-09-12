239ce61576e564762fa18eb023e0acd1d3495d72
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-12 17:37:09 +0300

    fix: return disabled button

--- components/rest-form/send-button/send-button.tsx
+++ components/rest-form/send-button/send-button.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Button } from '@/components/ui/button';
@@ -14,6 +15,7 @@ export const SendButton = ({ locale }: { locale: Locale }) => {
   const { body, headers, method, requestUrl } = useSelector((state: RootState) => state.restData);
 
   const router = useRouter();
+  const [isValid, setIsValid] = useState(!!(method && requestUrl));
 
   async function setUrl() {
     const query = setQueryParams(locale || 'en', method, requestUrl, body, headers);
@@ -32,20 +34,20 @@ export const SendButton = ({ locale }: { locale: Locale }) => {
   };
 
   async function handleOnClick() {
-    if (requestUrl) {
-      setUrl();
-      sendRequest();
-    }
+    setUrl();
+    sendRequest();
   }
+
+  useEffect(() => setIsValid(!!(method && requestUrl)), [method, requestUrl]);
 
   return (
     <>
       <div
         className={`rounded-md px-2 py-1 text-sm transition-colors duration-500 ${'bg-transparent text-red-600'}`}
       >
-        {!requestUrl && 'Please enter url'}
+        {!isValid && 'Please select HTTP method and enter url'}
       </div>
-      <Button onClick={handleOnClick} size="sm">
+      <Button disabled={!isValid} onClick={handleOnClick} size="sm">
         Send Request
       </Button>
     </>
