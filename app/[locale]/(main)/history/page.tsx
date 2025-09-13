b3a52bb4a926e9ffa35b8e4c4961cbd3d1d4f915
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-13 13:07:39 +0200

    feat: add message for empty request history

--- app/[locale]/(main)/history/page.tsx
+++ app/[locale]/(main)/history/page.tsx
@@ -1,10 +1,26 @@
 import { HistoryList } from '@components';
 
+import { Link } from '@/i18n/navigation';
 import { getUserRequestHistory, RequestLog } from '@/lib/history';
 
 export default async function History() {
-  const userId = 'H9MmBQpFF1N8Vc5HXw46oSzQVbg1';
+  const userId = '9OmzuIZaXkZzXKQ4wNEEGNArECU2';
   const history: RequestLog[] = await getUserRequestHistory(userId);
+
+  if (history.length === 0) {
+    return (
+      <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
+        <h3 className="mb-3 text-center text-[40px]">You haven't executed any requests yet</h3>
+        <div className="flex justify-center gap-2">
+          <p className="text-xl font-light">It's empty here. Try those options:</p>
+          {/* TODO: replace "/rest" with constant ROUTES.REST */}
+          <Link className="text-xl font-light underline" href="/rest">
+            Open REST Client
+          </Link>
+        </div>
+      </section>
+    );
+  }
 
   return (
     <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
