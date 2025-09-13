b941de81777b158725e127503de7e03d6ca3080c
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-13 17:22:52 +0300

    fix: add languages for footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,12 @@
+'use client';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import { AUTHOR_GITHUBS, RSS_LINK } from '@/constants';
 
 export const Footer = () => {
+  const t = useTranslations('footer');
   return (
     <footer className="bg-glass border-bg-300 text-muted w-full border-t py-1 text-center text-sm backdrop-blur-md">
       <div className="container mx-auto flex flex-col items-center justify-between px-6 py-1 md:flex-row">
@@ -15,15 +18,15 @@ export const Footer = () => {
               key={author.name}
               target="_blank"
             >
-              {author.name}
+              {t(author.name)}
             </Link>
           ))}
         </div>
 
-        <span className="text-sm text-gray-900">&copy; 2025 REST Client App </span>
+        <span className="text-sm text-gray-900">&copy; 2025 REST{t('client')} </span>
 
         <Link href={RSS_LINK} target="_blank">
-          <Image alt="RS School logo" height={40} src="/rss-logo.svg" width={40} />
+          <Image alt={t('logo')} height={40} src="rss-logo.svg" width={40} />
         </Link>
       </div>
     </footer>
