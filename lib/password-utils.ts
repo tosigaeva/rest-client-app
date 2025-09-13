7b5670c66a49c9690f0f830580846213571e3bbd
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-13 22:05:57 +0200

    refactor: validate passwords using unicode

--- lib/password-utils.ts
+++ lib/password-utils.ts
@@ -1,7 +1,7 @@
 export const PASSWORD_MIN_LENGTH = 8;
 
-export const hasUppercase = (value: string) => /[A-ZА-Я]/.test(value);
-export const hasLowercase = (value: string) => /[a-zа-я]/.test(value);
+export const hasUppercase = (value: string) => /\p{Lu}/u.test(value);
+export const hasLowercase = (value: string) => /\p{Ll}/u.test(value);
 export const hasNumber = (value: string) => /[0-9]/.test(value);
-export const hasSpecialChar = (value: string) => /[^A-Za-z0-9]/.test(value);
+export const hasSpecialChar = (value: string) => /[^\p{L}\p{N}]/u.test(value);
 export const hasMinLength = (value: string, length = PASSWORD_MIN_LENGTH) => value.length >= length;
