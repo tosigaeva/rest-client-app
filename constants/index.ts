30f1752b1f4cd3d0d4dcbc3020c37cb9da584f8c
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-14 08:29:21 +0300

    fix: add translatıons to About page

--- constants/index.ts
+++ constants/index.ts
@@ -1,39 +1,15 @@
 import { Header } from '@/type';
 
-export const AUTHOR_GITHUBS: { name: string; url: string }[] = [
-  { name: 'Anastasiia', url: 'https://github.com/tosigaeva' },
-  { name: 'Ekaterina', url: 'https://github.com/ekaterina-cat' },
-  { name: 'Yulia', url: 'https://github.com/YuliaDemir' },
+export const DEVELOPERS: { git: string; name: string }[] = [
+  { git: 'https://github.com/YuliaDemir', name: 'Yulia' },
+  { git: 'https://github.com/tosigaeva', name: 'Anastasiia' },
+  { git: 'https://github.com/ekaterina-cat', name: 'Katsiaryna' },
 ];
 
 export const RSS_LINK = 'https://rs.school/courses/reactjs';
 
-export const DEVELOPERS = [
-  {
-    description:
-      'Master of transforming code into culinary masterpieces. Each line is an ingredient in the perfect solution.',
-    git: 'https://github.com/YuliaDemir',
-    image: '/icon1.png',
-    major: 'Captain of the Kitchen | Team Lead, Developer',
-    name: 'Yulia',
-  },
-  {
-    description:
-      'Digital chef who turns complex problems into delicious solutions, serving innovation one line of code at a time.',
-    git: 'https://github.com/tosigaeva',
-    image: '/icon1.png',
-    major: 'Master Chef of Solutions, Developer',
-    name: 'Anastasiia',
-  },
-  {
-    description:
-      'Master of digital presentation, where every style is an ingredient of the perfect UI',
-    git: 'https://github.com/ekaterina-cat',
-    image: '/icon1.png',
-    major: 'Cooking Code, Developer',
-    name: 'Katsiaryna',
-  },
-];
+export const ICON = '/icon1.png';
+
 export const locales = ['en', 'ru', 'be'] as const;
 export type Locale = (typeof locales)[number];
 
