c404098988caf5209fbf698b839221eec62e3fc1
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-14 08:29:21 +0300

    fix: add translatıons to About page

--- components/about-us-page/about-us-page.tsx
+++ components/about-us-page/about-us-page.tsx
@@ -1,15 +1,17 @@
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import { GrGithub } from 'react-icons/gr';
 
-import { DEVELOPERS } from '@/constants';
+import { DEVELOPERS, ICON } from '@/constants';
 import { cn } from '@/lib/utils';
 
 export const AboutUsPage = () => {
+  const t = useTranslations('about');
   return (
     <section className="min-h-screen bg-white px-4 py-12 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-7xl">
         <h2 className="mb-12 text-center text-4xl font-bold tracking-tight capitalize">
-          a team that cooks with love
+          {t('team')}
         </h2>
         <div className="developers-grid">
           {DEVELOPERS.map((developer, index) => (
@@ -23,20 +25,26 @@ export const AboutUsPage = () => {
               key={developer.name}
             >
               <div className="relative overflow-hidden rounded-xl bg-gradient-to-br from-white to-gray-50 p-6">
-                <h4 className="mb-4 text-center text-2xl font-semibold">{developer.name}</h4>
+                <h4 className="mb-4 text-center text-2xl font-semibold">
+                  {t(`${developer.name}.name`)}
+                </h4>
                 <div className="mb-4 flex justify-center">
                   <div className="relative h-20 w-20 overflow-hidden rounded-full border-2 border-gray-200 shadow-md">
                     <Image
-                      alt={developer.name}
+                      alt={t(`${developer.name}.name`)}
                       className="absolute inset-0 h-full w-full object-cover"
                       height={80}
-                      src={developer.image}
+                      src={ICON}
                       width={80}
                     />
                   </div>
                 </div>
-                <p className="mb-2 text-center text-sm text-gray-600">Role: {developer.major}</p>
-                <p className="mb-6 text-center text-sm text-gray-800">{developer.description}</p>
+                <p className="mb-2 text-center text-sm text-gray-600">
+                  Role: {t(`${developer.name}.major`)}
+                </p>
+                <p className="mb-6 text-center text-sm text-gray-800">
+                  {t(`${developer.name}.descr`)}
+                </p>
                 <div
                   className={cn(
                     'mt-auto flex cursor-pointer items-center justify-center gap-2',
