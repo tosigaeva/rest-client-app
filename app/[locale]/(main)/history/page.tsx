bda7db7bb98a22d4181f6630da44e6e2d8274cc7
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-17 23:36:59 +0200

    feat: show history by authenticated user

--- app/[locale]/(main)/history/page.tsx
+++ app/[locale]/(main)/history/page.tsx
@@ -1,38 +1,18 @@
-import { HistoryList } from '@components';
-import { getTranslations } from 'next-intl/server';
+import { Suspense } from 'react';
 
-import { Link } from '@/i18n/navigation';
-import { getUserRequestHistory, RequestLog } from '@/lib/history';
+import { getCurrentUser } from '@/actions/auth-actions';
+import { HistoryList } from '@/components/history/list/history-list';
+import { Spinner } from '@/components/spinner';
 
 export default async function History() {
-  const t = await getTranslations('history');
-  const userId = 'H9MmBQpFF1N8Vc5HXw46oSzQVbg1';
-  const history: RequestLog[] = await getUserRequestHistory(userId);
-
-  if (history.length === 0) {
-    return (
-      <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
-        <h3 className="mb-3 text-center text-[40px]">{t('empty_title')}</h3>
-        <div className="flex justify-center gap-2">
-          <p className="text-xl font-light">{t('empty_description')}</p>
-          {/* TODO: replace "/rest" with constant ROUTES.REST */}
-          <Link className="text-xl font-light underline" href="/rest">
-            {t('open_rest_client')}
-          </Link>
-        </div>
-      </section>
-    );
-  }
+  const user = await getCurrentUser();
+  const userId = user?.uid;
 
   return (
-    <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
-      <div className="space-y-3 rounded-2xl border border-neutral-200 bg-neutral-50 p-4 shadow dark:bg-neutral-900">
-        <h3 className="text-lg font-semibold text-neutral-800 dark:text-neutral-100">
-          {t('title')}
-        </h3>
-        <HistoryList history={history} />
-        <p className="font-light text-gray-500">{t('description')}</p>
-      </div>
-    </section>
+    <div className="flex flex-col gap-6 rounded-2xl bg-white p-6">
+      <h2 className="mb-4 text-xl font-semibold text-gray-800 dark:text-gray-100">History</h2>
+      <p>Welcome, {user?.displayName || user?.email || 'User'}!</p>
+      <Suspense fallback={<Spinner />}>{userId && <HistoryList userId={userId} />}</Suspense>
+    </div>
   );
 }
--- components/history/list/history-list.tsx
+++ components/history/list/history-list.tsx
@@ -1,19 +1,41 @@
-'use client';
-
 import { HistoryItem } from '@components';
+import { getTranslations } from 'next-intl/server';
+import Link from 'next/link';
 
-import { RequestLog } from '@/lib/history';
+import { getUserRequestHistory, RequestLog } from '@/lib/history';
 
-type Props = {
-  history: RequestLog[];
-};
+export async function HistoryList({ userId }: { userId: string }) {
+  const t = await getTranslations('history');
+  const history: RequestLog[] = await getUserRequestHistory(userId);
 
-export function HistoryList({ history }: Props) {
+  if (history.length === 0) {
+    return (
+      <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
+        <h3 className="mb-3 text-center text-[40px]">{t('empty_title')}</h3>
+        <div className="flex justify-center gap-2">
+          <p className="text-xl font-light">{t('empty_description')}</p>
+          {/* TODO: replace "/rest" with constant ROUTES.REST */}
+          <Link className="text-xl font-light underline" href="/rest">
+            {t('open_rest_client')}
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
   return (
-    <ul className="flex flex-col gap-2">
-      {history.map((doc) => (
-        <HistoryItem doc={doc} key={doc.id} />
-      ))}
-    </ul>
+    <section className="m-full mx-auto mt-24 mb-auto px-8 pt-0 pb-8">
+      <div className="space-y-3 rounded-2xl border border-neutral-200 bg-neutral-50 p-4 shadow dark:bg-neutral-900">
+        <h3 className="text-lg font-semibold text-neutral-800 dark:text-neutral-100">
+          {t('title')}
+        </h3>
+        <ul className="flex flex-col gap-2">
+          {history.map((doc) => (
+            <HistoryItem doc={doc} key={doc.id} />
+          ))}
+        </ul>
+        <p className="font-light text-gray-500">{t('description')}</p>
+      </div>
+    </section>
   );
 }
