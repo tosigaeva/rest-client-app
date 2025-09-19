dc5fb92d832cbf7120194f05038f8f46dce11521
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-19 23:59:15 +0200

    style: update status color

--- components/history/item/history-item.tsx
+++ components/history/item/history-item.tsx
@@ -12,10 +12,12 @@ export async function HistoryItem({ doc }: Props) {
 
   const statusColor =
     doc.status >= 200 && doc.status < 300
-      ? 'bg-green-100 text-green-700'
-      : doc.status >= 400
-        ? 'bg-red-100 text-red-700'
-        : 'bg-yellow-100 text-yellow-700';
+      ? 'bg-green-100 text-green-600'
+      : doc.status >= 300 && doc.status < 400
+        ? 'bg-blue-100 text-blue-600'
+        : doc.status >= 400
+          ? 'bg-red-100 text-red-600'
+          : 'bg-gray-100 text-gray-600';
 
   const methodColor =
     doc.method === 'GET'
