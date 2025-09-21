4dc1758bfe2f0a2b97431bc8c818abd84b7cb2dd
Yulia  Demir <Nightelfin91@gmail.com>
2025-09-21 11:26:35 +0300

    fix: rest layout

---
+++ app/[locale]/(private)/rest/layout.tsx
@@ -0,0 +1,26 @@
+import { Body, GeneratedCode, RestHeaders, RestMain, SendButton } from '@components';
+import { redirect } from 'next/navigation';
+import { ReactNode } from 'react';
+
+import { getCurrentUser } from '@/actions/auth-actions';
+import { ROUTES } from '@/constants';
+
+export default async function RestClientLayout({ children }: { children: ReactNode }) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(ROUTES.MAIN);
+  }
+
+  return (
+    <div className="flex flex-col gap-6 rounded-2xl bg-white p-6">
+      <h2 className="mb-4 text-xl font-semibold text-gray-800 dark:text-gray-100">REST Client</h2>
+      <RestMain user={user} />
+      <RestHeaders user={user} />
+      <Body user={user} />
+      <GeneratedCode user={user} />
+      <SendButton user={user} />
+      {children}
+    </div>
+  );
+}
--- app/[locale]/(private)/rest/page.tsx
+++ app/[locale]/(private)/rest/page.tsx
@@ -1,4 +1,3 @@
-import { Body, GeneratedCode, RestHeaders, RestMain, SendButton } from '@components';
 import { redirect } from 'next/navigation';
 
 import { getCurrentUser } from '@/actions/auth-actions';
@@ -11,14 +10,5 @@ export default async function RestClientPage() {
     redirect(ROUTES.MAIN);
   }
 
-  return (
-    <div className="flex flex-col gap-6 rounded-2xl bg-white p-6">
-      <h2 className="mb-4 text-xl font-semibold text-gray-800 dark:text-gray-100">REST Client</h2>
-      <RestMain user={user} />
-      <RestHeaders user={user} />
-      <Body user={user} />
-      <GeneratedCode user={user} />
-      <SendButton user={user} />
-    </div>
-  );
+  return <></>;
 }
