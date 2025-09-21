cf0cc3b231b95fafe8a7ceece5b3dcc4bb089063
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-09-21 16:02:18 +0200

    chore: remove auth from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
-import { NextRequest } from 'next/server';
 
 import { routing } from './i18n/routing';
 
-const intlMiddleware = createMiddleware(routing);
-
-export default function middleware(request: NextRequest) {
-  return intlMiddleware(request);
-}
+export default createMiddleware(routing);
 
 export const config = {
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
